618f23ac7973e2621f5e11530110552ab24ec5d4
AlissonBarreiraDosReis <alisson.reis@escola.pr.gov.br>
2024-11-06 19:12:47 -0300

    atualizar

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -10,9 +10,6 @@ async function vizualizarInformacoesGlobais() {
     const minutos = Math.round((dados.tempo_medio_dia_estudando - horas) * 60)
     const pessoasComEducacaoSuperior = (dados.total_pessoas_com_educacao_superior / 1e9)
     const percentual = ((pessoasComAcessoAEducacao/pessoasMundo)*100).toFixed(2)
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o total de pessoas no mundo é de <span>${dados.total_pessoas_mundo}</span> de pessoas e que o  total de pessoas com acesso à educação é de <span>${dados.total_pessoas_com_acesso_a_educacao}</span>? O tempo médio gasto por dia estudando é de <span>${dados.tempo_medio_dia_estudando}</span> horas e o total de pessoas com educação superior é de <span>${dados.total_pessoas_com_educacao_superior}</span>.  Será que esses dados apontam para uma realidade de um mundo esclarecido e com facilidade de acesso à Educação?
 `
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo);
